eee6e9a50a9b426fe6f1c8bc4582b0b6bb16e702
jayfortier79 <jfortier05@yahoo.com>
2023-07-26 23:11:22 -0500

    fillingout

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-alert ("Hello")
+const audio1 = document.getElementById('classical');
+
+document.getElementById('mute').addEventListener('click', function (e)
+{
+    e = e || window;
+    audio1.muted = !audio1.muted;
+    e.preventDefault();
+}, false);
+
+const audio2 = document.getElementById('qmusic');
+
+document.getElementById('mute').addEventListener('click', function (e)
+{
+    e = e || window;
+    audio2.muted = !audio2.muted;
+    e.preventDefault();
+}, false);
+
+const audio3 = document.getElementById('lqmusic');
+
+document.getElementById('mute').addEventListener('click', function (e)
+{
+    e = e || window;
+    audio3.muted = !audio3.muted;
+    e.preventDefault();
+}, false);
+
+
+
+
+
